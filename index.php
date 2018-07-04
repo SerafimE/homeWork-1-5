be2af84328d45096bdbd22142a0811993ce08014
SerafimE <38590113+SerafimE@users.noreply.github.com>
2018-07-04 21:24:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -15,14 +15,14 @@ date_default_timezone_set('Europe/Moscow');
 </head>
 <body>
 <div>
-    <h1>Weather and forecasts in <?= $array_weather["name"] . ', ' . $array_weather["sys"]["country"] ?></h1>
+    <h1>Weather and forecasts in <?= $array_weather['name'] . ', ' . $array_weather['sys']['country'] ?></h1>
     <p id="date"><?= date('l j F Y') ?></p>
     <p id="date"><?= date('H:i') ?></p>
-    <p id="img"><img src="<?= 'http://openweathermap.org/img/w/' . $array_weather["weather"][0]["icon"] . '.png' ?>" alt=""></p>
-    <p class="condition"><?= 'temp: ' .  round($array_weather["main"]["temp"] - 273.15, 2) . ' °C' ?></p>
-    <p class="condition"><?= 'wind speed: ' . $array_weather["wind"]["speed"] . ' m/s' ?></p>
-    <p class="condition"><?= 'pressure: ' . $array_weather["main"]["pressure"] . ' gPa'?></p>
-    <p id="humidity"><?= 'Humidity: ' . $array_weather["main"]["humidity"] . '%' ?></p>
+    <p id="img"><img src="<?= 'http://openweathermap.org/img/w/' . $array_weather['weather'][0]['icon'] . '.png' ?>" alt=""></p>
+    <p class="condition"><?= 'temp: ' . round($array_weather['main']['temp'] - 273.15, 2) . ' °C' ?></p>
+    <p class="condition"><?= 'wind speed: ' . $array_weather['wind']['speed'] . ' m/s'?></p>
+    <p class="condition"><?= 'pressure: ' . $array_weather['main']['pressure'] . ' gPa' ?></p>
+    <p id="humidity"><?= 'Humidity: ' . $array_weather['main']['humidity'] . '%' ?></p>
 </div>
 </body>
 </html>
